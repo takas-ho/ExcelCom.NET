7ea200bbf19a1e6499d9e7456a28fb8bd312e3e0
HOMMA Takashi <hommatks@gmail.com>
2014-09-14 17:04:30 +0900

    基底クラスを追加する

---
+++ ExcelCom.Test/Core/AbstractExcelObjectTest.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Runtime.InteropServices
+Imports NUnit.Framework
+
+Namespace Core
+
+    Public MustInherit Class AbstractExcelObjectTest
+
+        Private Class TestingExcelObject : Inherits AbstractExcelObject
+
+            Public Sub New()
+                MyBase.New(CreateObject("Excel.Application"))
+            End Sub
+
+            Public Function GetProperty(ByVal name As String, ByVal ParamArray args As Object()) As Object
+                Return InvokeGetProperty(name, args)
+            End Function
+
+            Public Sub SetProperty(ByVal name As String, ByVal ParamArray args As Object())
+                InvokeSetProperty(name, args)
+            End Sub
+
+            Public Function DoMethod(ByVal name As String, ByVal ParamArray args As Object()) As Object
+                Return InvokeMethod(name, args)
+            End Function
+
+            Public Sub Close()
+                Marshal.FinalReleaseComObject(ComObject)
+            End Sub
+
+        End Class
+
+        Public Class [Default] : Inherits AbstractExcelObjectTest
+
+            Private sut As TestingExcelObject
+
+            <SetUp()> Public Sub SetUp()
+                sut = New TestingExcelObject
+            End Sub
+
+            <TearDown()> Public Sub TearDown()
+                sut.Close()
+            End Sub
+
+            <Test()> Public Sub InvokeGetProperty_プロパティ値を取得できる()
+                Assert.That(sut.GetProperty("Height"), [Is].GreaterThan(0))
+            End Sub
+
+            <Test()> Public Sub InvokeGetProperty_プロパティ値を取得できる_大文字小文字違いでも実行できる()
+                Assert.That(sut.GetProperty("height"), [Is].GreaterThan(0))
+            End Sub
+
+            <Test()> Public Sub InvokeSetProperty_プロパティ値を設定できる()
+                Assert.That(sut.GetProperty("Caption"), [Is].Not.EqualTo("HOGE"))
+
+                sut.SetProperty("Caption", "HOGE")
+
+                Assert.That(sut.GetProperty("Caption"), [Is].EqualTo("HOGE"))
+            End Sub
+
+            <Test()> Public Sub InvokeMethod_メソッドを実行できる()
+                Assert.That(sut.DoMethod("Evaluate", "=2*3"), [Is].EqualTo(6))
+            End Sub
+
+        End Class
+
+    End Class
+End Namespace
---
+++ ExcelCom/Core/AbstractExcelObject.vb
@@ -0,0 +1,91 @@
+﻿Namespace Core
+    ''' <summary>
+    ''' Excelオブジェクトの基底クラス
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public MustInherit Class AbstractExcelObject
+
+        ''' <summary>COMオブジェクト本体</summary>
+        Protected Friend ReadOnly ComObject As Object
+
+        Private ReadOnly comType As Type
+
+        Protected Sub New(ByVal comObject As Object)
+            Me.ComObject = comObject
+            Me.comType = comObject.GetType
+        End Sub
+
+        ''' <summary>
+        ''' プロパティ値を取得する
+        ''' </summary>
+        ''' <param name="name">プロパティ名</param>
+        ''' <param name="args">引数[]</param>
+        ''' <returns>値</returns>
+        ''' <remarks></remarks>
+        Protected Friend Function InvokeGetProperty(ByVal name As String, ByVal ParamArray args As Object()) As Object
+            Return InvokeGetProperty(Of Object)(name, args)
+        End Function
+
+        ''' <summary>
+        ''' プロパティ値を取得する
+        ''' </summary>
+        ''' <typeparam name="T">戻り値型</typeparam>
+        ''' <param name="name">プロパティ名</param>
+        ''' <param name="args">引数[]</param>
+        ''' <returns>値</returns>
+        ''' <remarks></remarks>
+        Protected Friend Function InvokeGetProperty(Of T)(ByVal name As String, ByVal ParamArray args As Object()) As T
+            Dim value As Object = comType.InvokeMember(name, Reflection.BindingFlags.GetProperty Or Reflection.BindingFlags.Public, Nothing, ComObject, ResolveArgs(args))
+            Return Cast(Of T)(value)
+        End Function
+
+        ''' <summary>
+        ''' プロパティ値を設定する
+        ''' </summary>
+        ''' <param name="name">プロパティ名</param>
+        ''' <param name="args">引数[]</param>
+        ''' <remarks></remarks>
+        Protected Friend Sub InvokeSetProperty(ByVal name As String, ByVal ParamArray args As Object())
+            comType.InvokeMember(name, Reflection.BindingFlags.SetProperty Or Reflection.BindingFlags.Public, Nothing, ComObject, ResolveArgs(args))
+        End Sub
+
+        ''' <summary>
+        ''' メソッドを実行する
+        ''' </summary>
+        ''' <param name="name">プロパティ名</param>
+        ''' <param name="args">引数[]</param>
+        ''' <returns>戻り値</returns>
+        ''' <remarks></remarks>
+        Protected Friend Function InvokeMethod(ByVal name As String, ByVal ParamArray args As Object()) As Object
+            Return InvokeMethod(Of Object)(name, args)
+        End Function
+
+        ''' <summary>
+        ''' メソッドを実行する
+        ''' </summary>
+        ''' <typeparam name="T">戻り値型</typeparam>
+        ''' <param name="name">プロパティ名</param>
+        ''' <param name="args">引数[]</param>
+        ''' <returns>戻り値</returns>
+        ''' <remarks></remarks>
+        Protected Friend Function InvokeMethod(Of T)(ByVal name As String, ByVal ParamArray args As Object()) As T
+            Dim value As Object = comType.InvokeMember(name, Reflection.BindingFlags.InvokeMethod Or Reflection.BindingFlags.Public, Nothing, ComObject, ResolveArgs(args))
+            Return Cast(Of T)(value)
+        End Function
+
+        Private Function Cast(Of T)(ByVal value As Object) As T
+            If GetType(T).IsEnum Then
+                Return DirectCast(DirectCast(CInt(value), Object), T)
+            End If
+            Return DirectCast(value, T)
+        End Function
+
+        Private Function ResolveArgs(ByVal args As Object()) As Object()
+            If args Is Nothing OrElse args.Length = 0 Then
+                Return Nothing
+            End If
+            Return args
+        End Function
+
+    End Class
+End Namespace
