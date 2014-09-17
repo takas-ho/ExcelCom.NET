35e9de7a764aa7c926232cbd792d33574e5db3a9
HOMMA Takashi <hommatks@gmail.com>
2014-09-17 21:48:32 +0900

    Addを実装する

--- ExcelCom.Test/Core/SheetsTest.vb
+++ ExcelCom.Test/Core/SheetsTest.vb
@@ -5,9 +5,11 @@ Namespace Core
     Public MustInherit Class SheetsTest
 
         Private sut As Application
+        Private workbook As Workbook
 
         <SetUp()> Public Sub SetUp()
             sut = New Application
+            workbook = sut.Workbooks.Add
         End Sub
 
         <TearDown()> Public Sub TearDown()
@@ -20,8 +22,32 @@ Namespace Core
         Public Class プロパティたちTest : Inherits SheetsTest
 
             <Test()> Public Sub Count_最初は0超_シート数はローカルPCの設定で変わる()
-                Dim workbook As Workbook = sut.Workbooks.Add
                 Assert.That(workbook.Sheets.Count, [Is].GreaterThan(0))
+            End Sub
+
+        End Class
+
+        Public Class Add_Test : Inherits SheetsTest
+
+            <Test()> Public Sub Addすれば1増える()
+                Dim baseCount As Integer = workbook.Sheets.Count
+                workbook.Sheets.Add()
+                Assert.That(workbook.Sheets.Count, [Is].EqualTo(baseCount + 1))
+            End Sub
+
+            <Test()> Public Sub Addしたら先頭に追加される()
+                Dim addedSheet As Worksheet = workbook.Sheets.Add()
+                addedSheet.Name = "XyZ"
+                Assert.That(workbook.Sheets(0).Name, [Is].EqualTo("XyZ"))
+            End Sub
+
+            <Test()> Public Sub Addすれば1増える222()
+                Dim firstSheet As Worksheet = workbook.Sheets.Add()
+
+                Dim actual As Worksheet = workbook.Sheets.Add(after:=firstSheet)
+                actual.Name = "ABC"
+
+                Assert.That(workbook.Sheets(1).Name, [Is].EqualTo("ABC"), "firstSheetの一つ後ろに追加")
             End Sub
 
         End Class
@@ -29,7 +55,6 @@ Namespace Core
         Public Class ExcelObjectたちTest : Inherits SheetsTest
 
             <Test()> Public Sub Worksheetが閉じられること()
-                Dim workbook As Workbook = sut.Workbooks.Add
                 Dim worksheet As Worksheet = workbook.Sheets.Item(0)
 
                 sut.Dispose()
--- ExcelCom/Core/Sheets.vb
+++ ExcelCom/Core/Sheets.vb
@@ -6,6 +6,23 @@
             MyBase.New(parent, comObject)
         End Sub
 
+        Public Function Add(Optional ByVal before As Worksheet = Nothing, Optional ByVal after As Worksheet = Nothing, Optional ByVal count As Integer = -1, Optional ByVal type As Object = Nothing) As Worksheet
+            Dim args As New List(Of Object)
+            If before IsNot Nothing Then
+                args.Add(New NamedParameter("Before", before.ComObject))
+            End If
+            If after IsNot Nothing Then
+                args.Add(New NamedParameter("After", after.ComObject))
+            End If
+            If 0 < count Then
+                args.Add(New NamedParameter("Count", count))
+            End If
+            If type IsNot Nothing Then
+                args.Add(New NamedParameter("Type", type))
+            End If
+            Return New Worksheet(Me, InvokeMethod("Add", args.ToArray))
+        End Function
+
         Public ReadOnly Property Count() As Integer
             Get
                 Return InvokeGetProperty(Of Integer)("Count")
