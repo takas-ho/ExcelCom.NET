b7e57e417e7637fe3b3d0063e52ec660aa12a026
HOMMA Takashi <hommatks@gmail.com>
2014-09-18 22:44:19 +0900

    Addのテストを追加する

--- ExcelCom.Test/Core/WorkbooksTest.vb
+++ ExcelCom.Test/Core/WorkbooksTest.vb
@@ -17,6 +17,28 @@ Namespace Core
             sut.Dispose()
         End Sub
 
+        Public Class Add_Test : Inherits WorkbooksTest
+
+            <Test()> Public Sub Addすれば1増える()
+                Dim baseCount As Integer = sut.Workbooks.Count
+                sut.Workbooks.Add()
+                Assert.That(sut.Workbooks.Count, [Is].EqualTo(baseCount + 1))
+            End Sub
+
+            <Test()> Public Sub Addしたら先頭に追加される()
+                Dim addedSheet As Workbook = sut.Workbooks.Add()
+                Assert.That(sut.Workbooks(0).Name, [Is].EqualTo(addedSheet.Name))
+            End Sub
+
+            <Test()> Public Sub Addを2度したら_2度目は後ろに追加される()
+                Dim firstSheet As Workbook = sut.Workbooks.Add()
+                Dim secondSheet As Workbook = sut.Workbooks.Add()
+                Assert.That(sut.Workbooks(0).Name, [Is].EqualTo(firstSheet.Name))
+                Assert.That(sut.Workbooks(1).Name, [Is].EqualTo(secondSheet.Name))
+            End Sub
+
+        End Class
+
         Public Class プロパティたちTest : Inherits WorkbooksTest
 
             <Test()> Public Sub Count_最初は0()
