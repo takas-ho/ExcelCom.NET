1b2b42bb39147725cb7b246091a5b3e990e20128
HOMMA Takashi <hommatks@gmail.com>
2014-09-24 21:35:21 +0900

    Fontを追加する

---
+++ ExcelCom.Test/Core/FontTest.vb
@@ -0,0 +1,76 @@
+﻿Imports NUnit.Framework
+
+Namespace Core
+
+    Public MustInherit Class FontTest
+
+        Private sut As Application
+        Private workbook As Workbook
+        Private sheet As Worksheet
+
+        <SetUp()> Public Sub SetUp()
+            sut = New Application
+            workbook = sut.Workbooks.Add
+            sheet = workbook.Sheets.Add
+        End Sub
+
+        <TearDown()> Public Sub TearDown()
+            If sut Is Nothing Then
+                Return
+            End If
+            sut.Dispose()
+        End Sub
+
+        'Public Class CellsTest : Inherits FontTest
+
+        '    <Test()> Public Sub Cellsの値をRangeと比較できる()
+        '        workbook.Sheets.Item(0).Cells(0, 1).Value = "abc"
+
+        '        Assert.That(workbook.Sheets.Item(0).Range("B1").Value, [Is].EqualTo("abc"))
+        '    End Sub
+
+        '    <Test()> Public Sub Cellsの値をRangeと比較できる2()
+        '        workbook.Sheets.Item(0).Cells(2, 0).Value = "aiueo"
+
+        '        Assert.That(workbook.Sheets.Item(0).Range("A3").Value, [Is].EqualTo("aiueo"))
+        '    End Sub
+
+        '    <Test()> Public Sub Hoge()
+        '        Dim start As Range = workbook.Sheets.Item(0).Cells(1, 1)
+
+        '        Dim target As Range = workbook.Sheets.Item(0).Range(start, start)
+        '        target.Value = "xyz"
+        '        Assert.That(workbook.Sheets.Item(0).Range("B2").Value, [Is].EqualTo("xyz"))
+        '    End Sub
+
+        'End Class
+
+        Public Class ExcelObjectたちTest : Inherits FontTest
+
+            '<Test()> Public Sub Cellsが閉じられること()
+            '    Dim cells As Range = workbook.Sheets(0).Shapes
+
+            '    sut.Dispose()
+            '    sut = Nothing
+
+            '    TestUtil.AssertNotExistsExcelPropcess()
+            'End Sub
+
+        End Class
+
+        Public Class PropertyたちTest : Inherits FontTest
+
+            <Test()> Public Sub Name_フォント名を設定する(<Values("MS P GOTHIC", "Arial", "Tahoma")> ByVal fontName As String)
+                sheet.Cells(2, 3).Font.Name = fontName
+                Assert.That(sheet.Cells(2, 3).Font.Name, [Is].EqualTo(fontName))
+            End Sub
+
+            <Test()> Public Sub Size_(<Values(10, 12.5, 14)> ByVal size As Double)
+                sheet.Cells(2, 3).Font.Size = size
+                Assert.That(sheet.Cells(2, 3).Font.Size, [Is].EqualTo(size))
+            End Sub
+
+        End Class
+
+    End Class
+End Namespace
---
+++ ExcelCom/Core/Font.vb
@@ -0,0 +1,27 @@
+﻿Namespace Core
+    Public Class Font : Inherits AbstractExcelSubObject : Implements IExcelObject
+
+        Public Sub New(ByVal parent As IExcelObject, ByVal comObject As Object)
+            MyBase.New(parent, comObject)
+        End Sub
+
+        Public Property Name() As String
+            Get
+                Return InvokeGetProperty(Of String)("Name")
+            End Get
+            Set(ByVal value As String)
+                InvokeSetProperty("Name", value)
+            End Set
+        End Property
+
+        Public Property Size() As Double
+            Get
+                Return InvokeGetProperty(Of Double)("Size")
+            End Get
+            Set(ByVal value As Double)
+                InvokeSetProperty("Size", value)
+            End Set
+        End Property
+
+    End Class
+End Namespace
