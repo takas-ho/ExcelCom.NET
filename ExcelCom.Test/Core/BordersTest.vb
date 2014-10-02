0be4ab1d18169be515deaede5a6f35b7cd1f7999
HOMMA Takashi <hommatks@gmail.com>
2014-10-02 18:06:00 +0900

    Colorもろもろを追加する

--- ExcelCom.Test/Core/BordersTest.vb
+++ ExcelCom.Test/Core/BordersTest.vb
@@ -1,4 +1,5 @@
 ﻿Imports NUnit.Framework
+Imports System.Reflection
 
 Namespace Core
 
@@ -38,6 +39,50 @@ Namespace Core
 
             <Test()> Public Sub Count_最初は0超()
                 Assert.That(sheet.Cells(3, 4).Borders.Count, [Is].GreaterThan(0))
+            End Sub
+
+            <Test()> Public Sub LineStyle_(<Values(Border.XlLineStyle.xlContinuous, Border.XlLineStyle.xlDash, Border.XlLineStyle.xlDashDot, Border.XlLineStyle.xlDashDotDot, Border.XlLineStyle.xlDot, Border.XlLineStyle.xlDouble, Border.XlLineStyle.xlLineStyleNone, Border.XlLineStyle.xlSlantDashDot)> ByVal index As Border.XlLineStyle)
+                With sheet.Cells(4, 5).Borders(Border.XlBordersIndex.xlEdgeTop)
+                    .LineStyle = index
+                    Assert.That(.LineStyle, [Is].EqualTo(index))
+                End With
+            End Sub
+
+            <Test()> Public Sub Color_Excel2003以前は56色(<Values(&HFFFFCC, &H99CCFF, &HFFCC99, &H663399)> ByVal color As Integer)
+                With sheet.Cells(4, 5).Borders(Border.XlBordersIndex.xlEdgeTop)
+                    .LineStyle = Border.XlLineStyle.xlDash
+                    .Color = color
+                    Assert.That(.Color, [Is].EqualTo(color))
+                End With
+            End Sub
+
+            <Test()> Public Sub ColorIndex_(<Values(1, 56, 20, 30)> ByVal index As Integer)
+                With sheet.Cells(4, 5).Borders(Border.XlBordersIndex.xlEdgeTop)
+                    .LineStyle = Border.XlLineStyle.xlContinuous
+                    .ColorIndex = index
+                    Assert.That(.ColorIndex, [Is].EqualTo(index))
+                End With
+            End Sub
+
+            <Test()> Public Sub ColorIndex_57以上はエラーになる(<Values(57, 100)> ByVal index As Integer)
+                Try
+                    With sheet.Cells(4, 5).Borders(Border.XlBordersIndex.xlEdgeTop)
+                        .LineStyle = Border.XlLineStyle.xlContinuous
+                        .ColorIndex = index
+                        Assert.That(.ColorIndex, [Is].EqualTo(index))
+                    End With
+                    Assert.Fail()
+                Catch expected As TargetInvocationException
+                    Assert.That(expected.InnerException.Message, [Is].EqualTo("Border クラスの ColorIndex プロパティを設定できません。"))
+                End Try
+            End Sub
+
+            <Test()> Public Sub Weight_(<Values(Border.XlBorderWeight.xlHairline, Border.XlBorderWeight.xlMedium, Border.XlBorderWeight.xlThick, Border.XlBorderWeight.xlThin)> ByVal weight As Border.XlBorderWeight)
+                With sheet.Cells(4, 5).Borders(Border.XlBordersIndex.xlEdgeTop)
+                    .LineStyle = Border.XlLineStyle.xlContinuous
+                    .Weight = weight
+                    Assert.That(.Weight, [Is].EqualTo(weight))
+                End With
             End Sub
 
         End Class
--- ExcelCom/Core/Borders.vb
+++ ExcelCom/Core/Borders.vb
@@ -33,5 +33,42 @@
                 Return New List(Of Border)(_items.Values)
             End Get
         End Property
+
+        Public Property Color() As Integer
+            Get
+                Return Convert.ToInt32(InvokeGetProperty("Color"))
+            End Get
+            Set(ByVal value As Integer)
+                InvokeSetProperty("Color", value)
+            End Set
+        End Property
+
+        Public Property ColorIndex() As Integer
+            Get
+                Return Convert.ToInt32(InvokeGetProperty("ColorIndex"))
+            End Get
+            Set(ByVal value As Integer)
+                InvokeSetProperty("ColorIndex", value)
+            End Set
+        End Property
+
+        Public Property LineStyle() As Border.XlLineStyle
+            Get
+                Return InvokeGetProperty(Of Border.XlLineStyle)("LineStyle")
+            End Get
+            Set(ByVal value As Border.XlLineStyle)
+                InvokeSetProperty("LineStyle", value)
+            End Set
+        End Property
+
+        Public Property Weight() As Border.XlBorderWeight
+            Get
+                Return InvokeGetProperty(Of Border.XlBorderWeight)("Weight")
+            End Get
+            Set(ByVal value As Border.XlBorderWeight)
+                InvokeSetProperty("Weight", value)
+            End Set
+        End Property
+
     End Class
 End Namespace
