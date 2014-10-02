e3751a8bff9834f4a8791438689f0917dffdcc98
HOMMA Takashi <hommatks@gmail.com>
2014-10-02 17:56:40 +0900

    LineStyleを追加する

--- ExcelCom/Core/Borders.vb
+++ ExcelCom/Core/Borders.vb
@@ -1,34 +1,12 @@
 ﻿Namespace Core
     Public Class Borders : Inherits AbstractExcelSubObject : Implements IExcelObject, IExcelCollection(Of Border)
 
-        Private ReadOnly _items As Dictionary(Of XlBordersIndex, Border)
-
-#Region "Xl定数"
-        ''' <summary>罫線の場所</summary>
-        Public Enum XlBordersIndex
-            ''' <summary>セル範囲の各セルの左上隅から右下隅への罫線</summary>
-            xlDiagonalDown = 5
-            ''' <summary>セル範囲の各セルの左下隅から右上隅への罫線</summary>
-            xlDiagonalUp = 6
-            ''' <summary>セル範囲の左側の罫線</summary>
-            xlEdgeLeft = 7
-            ''' <summary>セル範囲の上側の罫線</summary>
-            xlEdgeTop = 8
-            ''' <summary>セル範囲の下側の罫線</summary>
-            xlEdgeBottom = 9
-            ''' <summary>セル範囲の右側の罫線</summary>
-            xlEdgeRight = 10
-            ''' <summary>セル範囲の外枠を除く、すべてのセルの垂直方向の罫線</summary>
-            xlInsideVertical = 11
-            ''' <summary>セル範囲の外枠を除く、すべてのセルの水平方向の罫線</summary>
-            xlInsideHorizontal = 12
-        End Enum
-#End Region
+        Private ReadOnly _items As Dictionary(Of Border.XlBordersIndex, Border)
 
         Public Sub New(ByVal parent As IExcelObject, ByVal comObject As Object)
             MyBase.New(parent, comObject)
 
-            _items = New Dictionary(Of XlBordersIndex, Border)
+            _items = New Dictionary(Of Border.XlBordersIndex, Border)
         End Sub
 
         Public ReadOnly Property Count() As Integer
@@ -37,7 +15,7 @@
             End Get
         End Property
 
-        Default Public Overridable ReadOnly Property Item(ByVal index As XlBordersIndex) As Border
+        Default Public Overridable ReadOnly Property Item(ByVal index As Border.XlBordersIndex) As Border
             Get
                 If Not _items.ContainsKey(index) Then
                     Dim comObject As Object = InvokeGetProperty("Item", index)
