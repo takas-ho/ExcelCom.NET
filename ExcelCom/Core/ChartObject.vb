4ee296e898f36cac163517b69c705f23b05265b4
HOMMA Takashi <hommatks@gmail.com>
2014-10-12 07:54:07 +0900

    ChartObjectsを追加する

---
+++ ExcelCom/Core/ChartObject.vb
@@ -0,0 +1,24 @@
+﻿Namespace Core
+    Public Class ChartObject : Inherits AbstractExcelSubObject : Implements IExcelObject
+
+        Public Sub New(ByVal parent As IExcelObject, ByVal comObject As Object)
+            MyBase.New(parent, comObject)
+        End Sub
+
+        Public ReadOnly Property Index() As Integer
+            Get
+                Return RuleUtil.ConvIndexVBA2DotNET(InvokeGetProperty(Of Integer)("Index"))
+            End Get
+        End Property
+
+        Public Property Name() As String
+            Get
+                Return InvokeGetProperty(Of String)("Name")
+            End Get
+            Set(ByVal value As String)
+                InvokeSetProperty("Name", value)
+            End Set
+        End Property
+
+    End Class
+End Namespace
---
+++ ExcelCom/Core/ChartObjects.vb
@@ -0,0 +1,30 @@
+﻿Namespace Core
+    Public Class ChartObjects : Inherits AbstractExcelSubCollection(Of ChartObject) : Implements IExcelObject
+
+        Public Sub New(ByVal parent As IExcelObject, ByVal comObject As Object)
+            MyBase.New(parent, comObject)
+        End Sub
+
+        Protected Overrides Function DetectIndex(ByVal item As ChartObject) As Integer
+            Return item.Index
+        End Function
+
+        Protected Overrides Function DetectIndex(ByVal name As String) As Integer
+            For i As Integer = 0 To Me.Count - 1
+                If name.Equals(InternalItems(i).Name) Then
+                    Return i
+                End If
+            Next
+            Return -1
+        End Function
+
+        Public Function Add(ByVal Left As Double, ByVal Top As Double, ByVal Width As Double, ByVal Height As Double) As ChartObject
+            Dim comObject As Object = InvokeMethod("Add", Left, Top, Width, Height)
+            If comObject Is Nothing Then
+                Return Nothing
+            End If
+            Return New ChartObject(Me, comObject)
+        End Function
+
+    End Class
+End Namespace
